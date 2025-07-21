5cc612caed677bd8074179f8c717be73ab136e13
Lucas Almeida <lucas-lxx@alu.ufc.br>
2025-07-21 12:58:45 -0300

    chore: change the archtechture

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.use("/", (req, res, next) => {
-  res.json({ message: "works" });
+  res.json({ message: "work" });
 });
 
 app.listen(process.env.PORT ?? 3000);
