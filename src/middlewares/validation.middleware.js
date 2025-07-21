201ca15c724991e63b2742946de5f5a361a58a73
Lucas Almeida <lucas-lxx@alu.ufc.br>
2025-07-21 19:58:22 -0300

    fix(middleware/validation): fix the validation middleware to return the proper issues with the problematic fields

--- src/middlewares/validation.middleware.js
+++ src/middlewares/validation.middleware.js
@@ -3,7 +3,7 @@ function validate(schema) {
     const parse = schema.safeParse(req.body);
     if (!parse.success) {
       return res.status(400).json({
-        errors: parse.error.errors.map((err) => ({
+        errors: parse.error.issues.map((err) => ({
           field: err.path[0],
           message: err.message,
         })),
